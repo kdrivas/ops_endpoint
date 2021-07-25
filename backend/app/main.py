c0300150244f4df81a8ee7416b672585a9e4f9e9
kervyRivas <a20112128@pucp.pe>
2021-07-25 16:52:50 -0500

    adding pred_sample and pred_batch routes

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form, File, UploadFile
 from joblib import load
+from io import StringIO
+from starlette.middleware.cors import CORSMiddleware
+
+import codecs
+import csv
+import pandas as pd
+
+pipeline_pkl = load('pipeline.joblib')
 
 app = FastAPI()
-pipeline_pkl = load('pipeline.joblib')
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get('/')
 def root():
@@ -14,3 +28,24 @@ def get_dummy():
 	pred = pipeline_pkl.predict(sample)[0]
 
 	return {"pred_dummy": int(pred)}
+
+@app.post('/pred_sample/')
+def predict_sample(sepal_length:float = Form(...),
+									sepal_width:float = Form(...),
+									petal_length:float = Form(...),
+									petal_width:float = Form(...)):
+	sample = [[sepal_length, sepal_width, petal_length, petal_width]]
+	pred = pipeline_pkl.predict(sample)[0]
+
+	# To-do: Write in Redis
+
+	return {"prediction": int(pred)}
+
+@app.post('/pred_batch/')
+def predict_batch(file:UploadFile = File(...)):
+	data = pd.DataFrame(csv.reader(codecs.iterdecode(file.file, 'utf-8')))
+	preds = [float(v) for v in pipeline_pkl.predict(data)]
+
+	# To-do: Write in Redis
+
+	return {"predictions": preds}
